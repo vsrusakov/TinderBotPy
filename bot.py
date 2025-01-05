a3d4be1a1ce4f193c1db143fb5f41fda3346dbfa
virtp <virt.p.q@yandex.ru>
2025-01-05 20:06:19 +0300

    started telegram bot

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
-
+from local_settings import TELEGRAM_BOT_KEY
 from gpt import *
 from util import *
 
-# тут будем писать наш код :)
 
-
-app = ApplicationBuilder().token("telegram-token").build()
+app = ApplicationBuilder().token(TELEGRAM_BOT_KEY).build()
 app.run_polling()
